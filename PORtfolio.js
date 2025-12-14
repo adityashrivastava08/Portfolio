c48e7057a534c52698e9f5e7aa6d1b8486f065e0
adityashrivastava08 <adityaranjan2611@gmail.com>
2025-12-14 18:40:07 +0530

    Added portfolio files

---
+++ PORtfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: [ 'Web Developer', 'Java Developer', 'Problem Solver'],
+    typeSpeed: 150, 
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+});  
